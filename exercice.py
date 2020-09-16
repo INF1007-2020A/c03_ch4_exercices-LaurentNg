06934b4d01693a6142e56254e3499c17e6c900ee
LaurentNg <71021604+LaurentNg@users.noreply.github.com>
2020-09-16 10:45:41 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -13,7 +13,7 @@ def remove_third_char(string: str) -> str:
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return string [:2] + '' + string [3:]
+    return string [:2] + 'H' + string [3:]
 
 
 def get_nb_char(string: str, char: str) -> int:
